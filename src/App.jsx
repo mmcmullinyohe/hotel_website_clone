19fe4ff71fb1104106091192420460d9f0d94258
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-02 11:47:59 -0500

    hero image visible

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
 
@@ -8,9 +9,14 @@ const App = () => {
 
   return (
     <div>
-     {!isOwnerPath && <Navbar />}
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+      <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='flex flex-col items-start justify-center px-6 
-    md:px-16 lg:px-24 text-white bg-[ur("/src/assets/heroImage.png")] bg-no-repeat bg-cover
+    md:px-16 lg:px-24 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover
     bg-center h-screen'>
       
     </div>
