a5fd371676c014cb508e8b544831a7cf9b594219
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-02 11:41:30 -0500

    home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='flex flex-col items-start justify-center px-6 
-    md:px-16 lg:px-24 text-white'>
+    md:px-16 lg:px-24 text-white bg-[ur("/src/assets/heroImage.png")] bg-no-repeat bg-cover
+    bg-center h-screen'>
       
     </div>
   )
