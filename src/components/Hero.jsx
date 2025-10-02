61ed4549eadcf75a3e5b740a04cd316606b42e82
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-02 11:39:22 -0500

    create hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-start justify-center px-6 
+    md:px-16 lg:px-24 text-white'>
+      
+    </div>
+  )
+}
+
+export default Hero
