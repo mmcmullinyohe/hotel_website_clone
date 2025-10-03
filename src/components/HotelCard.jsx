7c4fb7ddbe34027f26bfd9e6dfd98a9fd23f6f49
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-03 15:59:31 -0500

    update

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const HotelCard = ({room, index}) => {
   return (
     <Link to={'/rooms/' + room.id} onClick={() => scrollTo(0, 0)} key={room._id}>
+        <img src={room.images[0]} alt="" />
+        <p>Best Seller</p>
+        <div>
+            <div>
+                <p>{room.hotel.name}</p>
+                <div className='flex items-center gap-1'>
+                    <img src={assets.starIconFilled} alt="star-icon" /> 4.5
+                </div>
+            </div>
+        </div>
     </Link> 
   )
 }
