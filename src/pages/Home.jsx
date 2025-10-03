64828bc4c67c7e9f3d93bff4031b882000411639
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-03 16:21:42 -0500

    hotel room samples

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import HotelCard from '../components/HotelCard'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedDestination />
     </>
   )
 }
