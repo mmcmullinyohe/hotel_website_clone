909f766b16aa6e009b56b553f959a8bf9f07d281
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-06 11:25:57 -0500

    imported exclusive offers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import HotelCard from '../components/HotelCard'
+import ExclusiveOffers from '../components/ExclusiveOffers'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedDestination />
+      <ExclusiveOffers />
     </>
   )
 }
